c7ef1cc1d8eefb3d9bea78165ccab34dd9785f4a
tainajmedeiros <tainajmedeiros@gmail.com>
2019-08-28 17:58:46 -0300

    fix: organizing the code connection

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -3,34 +3,32 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Connection {
 
-    private String url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=50cd7b778f02770f9b3bf8373315b74806f97082";
-    private String urlSolucition = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=50cd7b778f02770f9b3bf8373315b74806f97082";
-
+    // function responsible for the get connection
     public String getRequest() {
 
         HttpURLConnection connection = null;
 
         try {
+            String url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=50cd7b778f02770f9b3bf8373315b74806f97082";
             URL myurl = new URL(url);
             connection = (HttpURLConnection) myurl.openConnection();
 
             connection.setRequestMethod("GET");
 
-            StringBuilder content;
+            StringBuilder content = new StringBuilder();
 
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(connection.getInputStream()));
 
             String line;
-            content = new StringBuilder();
 
+            // dynamically manipulating string data
             while ((line = in.readLine()) != null) {
                 content.append(line);
                 content.append(System.lineSeparator());
@@ -42,21 +40,23 @@ public class Connection {
             Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
 
+            assert connection != null;
             connection.disconnect();
         }
         return null;
     }
 
-    public String postRequest(String file_name_with_ext, byte[] byteArray){
+    // function responsible for the post connection
+    public String postRequest(String name, byte[] byteArray){
 
-        String attachmentName = "answer";
-        String attachmentFileName = file_name_with_ext;
+        String file = "answer";
         String crlf = "\r\n";
         String twoHyphens = "--";
         String boundary =  "*****";
 
         try{
 
+            String urlSolucition = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=50cd7b778f02770f9b3bf8373315b74806f97082";
             URL url = new URL(urlSolucition);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
@@ -68,8 +68,8 @@ public class Connection {
                     connection.getOutputStream());
             request.writeBytes(twoHyphens + boundary + crlf);
             request.writeBytes("Content-Disposition: form-data; name=\"" +
-                    attachmentName + "\";filename=\"" +
-                    attachmentFileName + "\"" + crlf);
+                    file + "\";filename=\"" +
+                    name + "\"" + crlf);
             request.writeBytes(crlf);
             request.write(byteArray);
             request.writeBytes(crlf);
@@ -78,15 +78,15 @@ public class Connection {
             request.flush();
             request.close();
 
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
 
             // checks server's status code first
             int status = connection.getResponseCode();
             if (status == HttpURLConnection.HTTP_OK) {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(
                         connection.getInputStream()));
-                String line = null;
-                while ((line = reader.readLine()) != null) {
+                String line;
+                while (null != (line = reader.readLine())) {
                     response.append(line);
                 }
                 reader.close();
